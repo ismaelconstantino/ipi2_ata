38c5a3ae23a2e95c0dd4cad1e91923b535d26868
ismaelconstantino <iscon.piacatu@gmail.com>
2025-01-20 23:57:16 -0300

    menu "sobre" finalizado

--- static/script.js
+++ static/script.js
@@ -9,5 +9,16 @@ menuMobile.addEventListener('click', () => {
     body.classList.toggle("menu-nav-active");
 });
 
+/* Fecha menu quando clicar em algum item, alterando icone para list */
 
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+});
 
